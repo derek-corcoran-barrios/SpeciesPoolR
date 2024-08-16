963f504ff65f3980e2d3ca35ca0abf64aab58b3f
derek-corcoran-barrios <derek-corcoran-barrios@gmail.com>
2024-08-16 20:33:56 +0200

    Fixed run_workflow up to filter within

--- R/run_workflow.R
+++ R/run_workflow.R
@@ -7,36 +7,43 @@
 #' @param workers Number of parallel workers to use in the `crew` controller. Default is 2.
 #' @param error Handling for errors in outdated targets. Default is "null".
 #' @param file_path Path to the Excel or csv file containing the data.
+#' @param filter An optional expression used to filter the resulting `data.frame`. This should be an expression
+#' written as if you were using `dplyr::filter()`. The default is NULL, meaning no filtering is applied.
 #' @return Executes the `targets` pipeline.
 #' @importFrom crew crew_controller_local
-#' @importFrom targets tar_option_set tar_target tar_script
+#' @importFrom rlang enquo
+#' @importFrom targets tar_option_set tar_target tar_helper tar_make
 #' @export
+
 run_workflow <- function(workers = 2,
                          error = "null",
-                         file_path) {
+                         file_path,
+                         filter = NULL
+                         ) {
 
-  data <- Clean <- clean_species <- NULL
+  data <- Clean <- NULL
 
-  controller <- substitute(crew::crew_controller_local(workers = workers))
-  error_val <- substitute(error)
-  file_path_val <- substitute(file_path)
+  # Write the script using tar_helper()
+  targets::tar_helper(
+    path = "_targets.R",
+    code = {
 
-  # Set tar_option_set for the workflow
-  targets::tar_script({
-    targets::tar_option_set(
-      packages = c("SpeciesPoolR"),
-      controller = eval(controller),
-      error = eval(error_val)
-    )
+      targets::tar_option_set(
+        packages = c("SpeciesPoolR"),
+        controller =  crew::crew_controller_local(workers = !!workers),
+        error = !!error
+      )
 
-    # Define the targets pipeline
-    targets <- list(
-      targets::tar_target(file, command = eval(file_path_val), format = "file"),
-      targets::tar_target(data, get_data(file)),
-      targets::tar_target(Clean, clean_species(data))
-    )
-  }, ask = FALSE)
+      list(
+        targets::tar_target(file, command = !!file_path, format = "file"),
+        targets::tar_target(data, get_data(file, filter = !!rlang::enquo(filter))),
+        targets::tar_target(Clean, SpeciesPoolR::Clean_Taxa(data$Species))
+      )
+    },
+    tidy_eval = TRUE  # This ensures the !! operators work as expected
+  )
 
   # Run the workflow
-  targets::tar_make(targets)
+  targets::tar_make()
 }
+
